9d5812338c57a7136ee8078bdad6da89bf01253d
ebenofere <eofere@gmail.com>
2025-08-17 00:35:49 -0400

    first commit of the century

--- keeps.js
+++ keeps.js
@@ -1,3 +1,4 @@
+// database
 let Africa = {
   id: 13,
   country: "Nigeria",
@@ -9,3 +10,39 @@ let Africa = {
 };
 
 console.log(!Africa.isRich);
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// a program to determine suffrage eligibility
+function eligibleToVote(age) {
+  if (age < 0) {
+    console.log("Invalid input");
+  } else if (age < 18) {
+    console.log("Not eligible to vote");
+  } else {
+    console.log("Eligible to vote");
+  }
+}
+
+eligibleToVote(-1);
+eligibleToVote(0);
+eligibleToVote(15);
+eligibleToVote(50);
+
+// ------------------------------------------------------------------------------------------------------------------
+
+//  create a function to check if a number is even or odd
+
+function isEvenOrOdd(num) {
+  if (num == 0) {
+    console.log("this number is 0; and therefore is neither even nor odd");
+  } else if (num % 2 == 0) {
+    console.log("this number is an even number");
+  } else {
+    console.log("this number is an odd number");
+  }
+}
+isEvenOrOdd(-1);
+isEvenOrOdd(0);
+isEvenOrOdd(5);
+isEvenOrOdd(8);
--- practice.js
+++
@@ -1,9 +0,0 @@
-function eligibleToVote(age) {
-  if (age < 18) {
-    console.log("Not eligible to vote");
-  } else {
-    console.log("Eligible to vote");
-  }
-}
-
-eligibleToVote(-1);
