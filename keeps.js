011648107b52d2188400ea61bbd58fb4ee3eadb4
ebenofere <eofere@gmail.com>
2025-08-17 00:12:39 -0400

    first commit of the century

---
+++ keeps.js
@@ -0,0 +1,11 @@
+let Africa = {
+  id: 13,
+  country: "Nigeria",
+  capital: "Abuja",
+  calling_code: "+234",
+  demonym: "NGA",
+  president: "Bola Ahmed Tinubu",
+  isRich: false,
+};
+
+console.log(!Africa.isRich);
---
+++ practice.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+  let sum = a + b;
+  console.log(sum);
+}
+
+let x = 7;
+let y = 6;
+
+sum(x, y);
