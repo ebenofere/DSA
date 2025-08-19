5fb408f14b6723839b1ba9d649734d082d394261
ebenofere <eofere@gmail.com>
2025-08-19 00:42:15 -0400

    daily commit

--- keeps.js
+++ keeps.js
@@ -68,3 +68,22 @@ for (i = 0; i < arr.length; i++) {
     console.log(`this number ${arr[i]} is an even number`);
   }
 }
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Write a function that searches for an element in an array and returns the index.
+// If the element is not present, then just return -1
+
+function searchElement(arr, num) {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] == num) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let num = 4;
+let elementFinder = searchElement(arr, num);
+console.log(elementFinder);
--- practice.js
+++ practice.js
@@ -1,15 +1,16 @@
 // Write a function that searches for an element in an array and returns the index.
 // If the element is not present, then just return -1
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
 function searchElement(arr, num) {
   for (i = 0; i < arr.length; i++) {
-    if (arr[num] == num) {
-      console.log(`the index of ${arr[i]} is ${i} `);
-    } else {
-      console.log("-1");
+    if (arr[i] == num) {
+      return i;
     }
   }
+  return -1;
 }
-searchElement(arr, 0);
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let num = 4;
+let elementFinder = searchElement(arr, num);
+console.log(elementFinder);
