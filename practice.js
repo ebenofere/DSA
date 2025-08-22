0bf1e3c1495186a653b109a6b261caa636ed4d57
ebenofere <eofere@gmail.com>
2025-08-21 23:56:42 -0400

    update

--- practice.js
+++ practice.js
@@ -1,7 +1,7 @@
 // loop within a loop
 
-for (i = 0; i < 5; i++) {
-  for (j = 0; j < 5; j++) {
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; j < 5; j++) {
     console.log(`i is ${i} and j is ${j}`);
   }
 }
