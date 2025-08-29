9ed39f6562e42eb5f94be3ff5d5535013c1ab10d
ebenofere <eofere@gmail.com>
2025-08-29 05:44:15 -0400

    daily commit

---
+++ 1.warmup.py
@@ -0,0 +1,15 @@
+# sum of two integers
+def sum(a, b):
+    add = a + b
+    print(add)
+
+sum(5, 3)
+
+# -------------------------------------------------------------------
+
+# Square of a Number
+def square(x):
+    return x * x
+
+result = square(3)
+print("The square is:", result)
